4ae67cc867168debdd944cdff64f642423e17ff2
KateM_zz <zupperu@gmail.com>
2024-01-28 20:13:55 +0300

    models and settings

--- innoter/settings.py
+++ innoter/settings.py
@@ -9,18 +9,22 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+import environ
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+environ.Env.read_env(env_file=BASE_DIR / ".env")
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4ti0kp7twohdwtb49w@$3+w$r-o5ekt!&po8z7d$xi@om2+9#@"
+
+# Raises Django's ImproperlyConfigured
+# exception if SECRET_KEY not in os.environ
+SECRET_KEY = env('SECRET_KEY')
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +41,14 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third party packages
     'rest_framework',
+    # Internal
+    "pages",
+    "posts"
+
+
+
 ]
 
 MIDDLEWARE = [
@@ -75,12 +86,19 @@ WSGI_APPLICATION = "innoter.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# settings.py
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST', default="localhost"),
+        'PORT': env('DB_PORT')
     }
 }
+
 
 
 # Password validation
---
+++ pages/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+
+class Tag(models.Model):
+    """
+    Model for representing tags.
+    """
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    user_id = models.UUIDField(editable=False)
+    image_url = models.URLField()
+    tags = models.ManyToManyField('Tag', related_name='pages')
+    followers = models.ManyToManyField('Followers', related_name='following_pages')
+    is_blocked = models.BooleanField(default=False)
+    unblock_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Followers(models.Model):
+    page_id = models.ForeignKey(Page, on_delete=models.CASCADE)
+    user_id = models.UUIDField(editable=False)
